1c3b52a6665b064698d516db09aa3154c2292a25
Floflote <florent13a@gmail.com>
2023-03-17 10:27:53 +0100

    Sauv-before-adding-session

--- Website/Page/Espaceconnexion-content.php
+++ Website/Page/Espaceconnexion-content.php
@@ -6,7 +6,8 @@
     background-repeat: no-repeat;
     background-size: cover;">
   <div class="text-center p-5">
-    <h1 style="font-size: 60px; color: white; text-transform: uppercase; paint-order: stroke fill; stroke-color: #a4872c; stroke-width: 5px;">
+    <h1
+      style="font-size: 60px; color: white; text-transform: uppercase; paint-order: stroke fill; stroke-color: #a4872c; stroke-width: 5px;">
       Espace connexion
     </h1>
   </div>
@@ -18,13 +19,16 @@
 <section class="connect_compte_section p-5">
   <div class="container-fluid text-center">
     <h1>Se connecter à votre compte</h1>
-    <form method="POST" id="formconnect" class="needs-validation" action="Espaceconnexion.php" enctype="multipart/form-data" novalidate>
+    <form method="POST" id="formconnect" class="needs-validation" action="Espaceconnexion.php"
+      enctype="multipart/form-data" novalidate>
 
       <!-- Mail -->
 
       <div class="row col-sm-4 mx-auto" style="margin-bottom: 1rem;">
         <label for="connect_email" class="form-label">Identifiant (mail)</label>
-        <input type="email" class="form-control" pattern="(\w+\.?|-?\w+?)+@\w+\.?-?\w+?(\.\w{2,3})+" value="<?php echo (isset($_POST['connect_email'])) ? htmlspecialchars($_POST['connect_email']) : '' ?>" placeholder="monmail@mail.com" name="connect_email" id="connect_email" required>
+        <input type="email" class="form-control" pattern="(\w+\.?|-?\w+?)+@\w+\.?-?\w+?(\.\w{2,3})+"
+          value="<?php echo (isset($_POST['connect_email'])) ? htmlspecialchars($_POST['connect_email']) : '' ?>"
+          placeholder="monmail@mail.com" name="connect_email" id="connect_email" required>
         <div class="invalid-feedback">
           Vous devez entrer une adresse mail valide !
         </div>
@@ -36,7 +40,10 @@
         <label for="connect_password" class="form-label">
           Mot de passe
         </label>
-        <input type="password" class="form-control" pattern="^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){8,16}$" value="<?php echo (isset($_POST['connect_password'])) ? htmlspecialchars($_POST['connect_password']) : '' ?>" placeholder="************" name="connect_password" id="connect_password" required>
+        <input type="password" class="form-control"
+          pattern="^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){8,16}$"
+          value="<?php echo (isset($_POST['connect_password'])) ? htmlspecialchars($_POST['connect_password']) : '' ?>"
+          placeholder="************" name="connect_password" id="connect_password" required>
         <div class="invalid-feedback">
           Vous devez entrer un mot de passe valide !
         </div>
@@ -56,10 +63,10 @@
 
           if ($compte_customer === false) {
       ?>
-            <div style="color: #b02a37; margin-bottom: 1rem;">
-              Identifiant invalide !
-            </div>
-            <?php
+      <div style="color: #b02a37; margin-bottom: 1rem;">
+        Identifiant et / ou mot de passe invalide !
+      </div>
+      <?php
 
             /* Verify password */
 
@@ -67,20 +74,20 @@
             if (password_verify($connect_password, $compte_customer['customer_password'])) {
             ?>
 
-              <!-- Connexion validé -->
+      <!-- Connexion validé -->
 
-              <script type="text/javascript">
-                swal("Connecté", "Vous pouvez dès maintenant l'utiliser !", "success").then((value) => {
-                  window.location.replace("Reserve-table.php");
-                });
-              </script>
+      <script type="text/javascript">
+      swal("Connecté", "Vous pouvez dès maintenant l'utiliser !", "success").then((value) => {
+        window.location.replace("Reserve-table.php");
+      });
+      </script>
 
-            <?php
+      <?php
             } else {
             ?>
-              <div style="color: #b02a37; margin-bottom: 1rem;">
-                Identifiant invalide ou mdp !
-              </div>
+      <div style="color: #b02a37; margin-bottom: 1rem;">
+        Identifiant et / ou mot de passe invalide !
+      </div>
       <?php
             }
           }
@@ -91,8 +98,9 @@
       ?>
 
       <div style="margin-bottom: 1rem;">
-        <button type="submit" name="submit_connect_form" class="btn reserve-btn ms-3" style="text-transform: uppercase;">
-          Créer le compte
+        <button type="submit" name="submit_connect_form" class="btn reserve-btn ms-3"
+          style="text-transform: uppercase;">
+          Se connecter
         </button>
       </div>
     </form>
